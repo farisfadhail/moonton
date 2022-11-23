f9425fa2b9863ace674240be0e9cdccb695dce0b
Faris Fadhail <farisfadhail@gmail.com>
2022-11-23 23:16:24 +0700

    feat: admin update movie

--- app/Http/Controllers/Admin/MovieController.php
+++ app/Http/Controllers/Admin/MovieController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\Admin\Movie\Store;
+use App\Http\Requests\Admin\Movie\Update;
 
 class MovieController extends Controller
 {
@@ -18,7 +19,10 @@ class MovieController extends Controller
      */
     public function index()
     {
-        return inertia('Admin/Movie/Index');
+        $movies = Movie::all();
+        return inertia('Admin/Movie/Index', [
+            'movies' => $movies
+        ]);
     }
 
     /**
@@ -69,7 +73,9 @@ class MovieController extends Controller
      */
     public function edit(Movie $movie)
     {
-        //
+        return inertia('Admin/Movie/Edit', [
+            'movie' => $movie
+        ]);
     }
 
     /**
@@ -79,9 +85,23 @@ class MovieController extends Controller
      * @param  \App\Models\Movie  $movie
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Movie $movie)
+    public function update(Update $request, Movie $movie)
     {
-        //
+        $data = $request->validated();
+
+        if ($request->file('thumbnail')) {
+            $data['thumbnail'] = Storage::disk('public')->put('movies', $request->file('thumbnail'));
+            Storage::disk('public')->delete(($movie->thumbnail));
+        } else {
+            $data['thumbnail'] = $movie->thumbnail;
+        }
+
+        $movie->update($data);
+
+        return redirect(route('admin.dashboard.movie.index'))->with([
+            'message' => 'Movie updated successfully',
+            'type' => 'success'
+        ]);
     }
 
     /**
